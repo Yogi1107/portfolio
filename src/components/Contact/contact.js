588e9f74e650d9acb2c130a6170eff9fbc6fd3aa
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2026-01-28 22:49:05 +0530

    Updated

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,16 +2,34 @@ import React from 'react';
 import linkedin from '../../assets/l.png';
 import github from '../../assets/g.png';
 import leetcode from '../../assets/leetcode.png';
+import emailjs from '@emailjs/browser';
 import './contact.css'
 
 export default function Contact() {
+    const form = useRef();
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_5cfieju', 'template_6rd8xll', form.current, {
+        publicKey: '7T12GZHlNEGm2f2swDkhe',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+    };
     return (
         <div id='contact'>
             <h1 className='contactPageTitle'>Contact Me</h1>
             <span className='contactDesc'>Feel free to reach out if you’d like to connect, collaborate, or discuss opportunities.</span>
-            <form className='contactForm'>
-                <input type='text' className='name' placeholder='Your Name'/>
-                <input type='email' className='email' placeholder='Your Email'/>
+            <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                <input type='text' className='name' placeholder='Your Name' name='name'/>
+                <input type='email' className='email' placeholder='Your Email' name='from_email'/>
                 <textarea className='msg' name='message' rows='5' placeholder='Your message'></textarea>
                 <button className='submitBtn' type='submit' value='Send'>Submit</button>
                 <div className='links'>
